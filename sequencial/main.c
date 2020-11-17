429349072f41c83a1cdb86d30d51fd54311aaf02
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-11-17 21:43:33 +0000

    add quicksort and proper makefile

--- sequencial/main.c
+++ sequencial/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define NUMBER_BUCKET 10
+#define N_BUCKETS 10
 
 struct Bucket {
     int* array;
@@ -19,28 +19,28 @@ struct Bucket* make(size_t max){
     return block_arr;
 }
 
+void free_bucket(struct Bucket* b){
+    free(b->array);
+    free(b);
+}
+
 //This could write in res with no limits
 void buckets_to_arr(size_t size, struct Bucket* arr[size], int* res) {
     size_t w = 0;
     for(size_t bucket = 0; bucket < size; bucket++) {
-        printf("%zu ", arr[bucket]->n_elem);
+        #ifdef NDEBUG
+        printf("%zu: ", arr[bucket]->n_elem);
+        #endif
         for(size_t elem = 0; elem < arr[bucket]->n_elem; elem++) {
             res[w++] = arr[bucket]->array[elem];
+            #ifdef NDEBUG
             printf("%d ", arr[bucket]->array[elem]);
+            #endif
         }
+        #ifdef NDEBUG
         printf("\n");
+        #endif
     }
-}
-
-int array_max(int* arr, size_t size) {
-    if(size > 0) {
-        int curr_max = arr[0];
-        for(size_t i = 1; i < size; i++)
-            if(arr[i] > curr_max) curr_max = arr[i];
-        return curr_max;
-    }
-    fprintf(stderr, "Can't calculate max of empty array");
-    _exit(1);
 }
 
 void insert(struct Bucket* arr, int elem){
@@ -48,45 +48,79 @@ void insert(struct Bucket* arr, int elem){
         arr->array = realloc(arr->array, sizeof(int) * arr->max_elem * 2);
         arr->max_elem *= 2;
     }
-
     arr->array[arr->n_elem] = elem;
     arr->n_elem++;
 }
 
-void bucket_sort(size_t size, int* arr){
-    int max = array_max(arr, size);
 
-    struct Bucket* buckets[NUMBER_BUCKET];
-    for (size_t i = 0; i < NUMBER_BUCKET; i++){
-        buckets[i] = make(size);
+int cmpfunc (const void * a, const void * b) {
+    return ( *(int*)a - *(int*)b );
+}
+
+void bucket_sort(size_t size, int* arr){
+    if (!size){
+        fprintf(stderr, "Can't calculate max of empty array");
+        _exit(1);
     }
 
+    int max = arr[0];
+    int min = arr[0];
+    for(size_t i = 1; i < size; i++) {
+        if(arr[i] > max) max = arr[i];
+        if(arr[i] < min) min = arr[i];
+    }
+
+    #ifdef NDEBUG
+    printf("max: %d, min: %d\n", max, min);
+    #endif
+
+    struct Bucket* buckets[N_BUCKETS];
+    for (size_t i = 0; i < N_BUCKETS; i++)
+        buckets[i] = make(size);
+
     for (size_t i = 0; i < size; i++){
-        int n_bucket = max == 0 ? 0 : NUMBER_BUCKET * arr[i] / max;
-        n_bucket = n_bucket == NUMBER_BUCKET ? n_bucket - 1 : n_bucket;
+        size_t n_bucket = arr[i] * N_BUCKETS / (max - min);
+        n_bucket = n_bucket == N_BUCKETS ? n_bucket - 1 : n_bucket;
+
         insert(buckets[n_bucket], arr[i]);
     }
 
-    for (size_t i = 0; i < NUMBER_BUCKET; i++){
-        if(buckets[i]->n_elem > 1)
-            bucket_sort(buckets[i]->n_elem, buckets[i]->array);
-    }
+    for (size_t i = 0; i < N_BUCKETS; i++)
+        qsort(buckets[i]->array, buckets[i]->n_elem, sizeof(int), cmpfunc);
 
-    buckets_to_arr(NUMBER_BUCKET, buckets, arr);
-} 
+    buckets_to_arr(N_BUCKETS, buckets, arr);
+
+    for (size_t i = 0; i < N_BUCKETS; i++)
+        free_bucket(buckets[i]);
+
+}
+
+void print_arr(int* arr, size_t size){
+    for(size_t i = 0; i < size; i++) {
+        arr[i] = arr[i];
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 
 int main(){
-    int* arr = malloc(sizeof(int) * 20);
-    int arri[20] = {1, 10, 2, 35, 12, 4, 89, 131, 30, 0, 20, 11, 3, 36, 13, 5, 90, 14, 44, 55};
+    static size_t size = 1000000;
 
-    for(size_t i = 0; i < 20; i++) {
-        arr[i] = arri[i];
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    bucket_sort(20, arr);
-    for(size_t i = 0; i < 20; i++) 
-        printf("%d ", arr[i]);
-    printf("\n");
+    struct Bucket* b = make(size);
+    for (size_t x = size; x > 0; x--)
+        insert(b, x);
+
+    #ifdef NDEBUG
+    print_arr(b->array, size);
+    #endif
+
+    bucket_sort(size, b->array);
+
+    #ifdef NDEBUG
+    print_arr(b->array, size);
+    #endif
+
+    free_bucket(b);
 
 }
